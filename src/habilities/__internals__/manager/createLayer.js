b0baa1bd33b40c02ce0edd0d752527ec02a2692a
Eduardo Anaya <gran_ed@hotmail.com>
2018-12-09 15:57:38 +0100

    Add more meaningful readme

--- src/habilities/__internals__/manager/createLayer.js
+++ src/habilities/__internals__/manager/createLayer.js
@@ -2,16 +2,32 @@ const file = require('../common/fs-manager');
 const templates = require('./templates');
 const generateFactory = require('../common/generate');
 
-// layer: 'useCases',
-// [{
-//   name: 'somthing'
-//   placeHolderList: [
-//     { regex: /<ENTITY_NAME>/g, value: name },
-//     { regex: /<ENTITY_PROPS>/g, value: properties },
-//   ]
-// }]
-
-// NOTE: We can pass the template in a later stage
+/**
+ * This function is in charge to create the files base on an specified layer(entities, interactors,
+ * stores, usecases). Recieves a list of values containing a name to be use for the file and the
+ * placeholders to replace into the template.
+ *
+ * @example
+ * given values:
+ * layer = 'entities'
+ * path = '/my/path'
+ * valueMapper = [ {
+ *  name: 'Person',
+ *  placeHolderList: [
+ *    { regex: /<ENTITY_NAME>/g, value: 'Person' }
+ *  ]
+ * } ],
+ * template = 'class <ENTITY_NAME> {}'
+ *
+ * output: /my/path/Person.js
+ * content: 'class Person {}'
+ *
+ * @param {*} layer Layer to work. (entities, stores, interactors, useCases)
+ * @param {*} path Path where the file(s) will be generated
+ * @param {*} valueMapper List of values to be appended to the file
+ * @param {*} template Template to use to generate file content
+ * @returns A list of names of files that where successfuly created.
+ */
 function createLayerFiles(layer, path, valueMapper, template) {
   const generatedFileList = [];
   const generate = generateFactory(template);
