c0032775ddd97edc2769b609889b7a96ebe8f497
Eduardo Anaya <gran_ed@hotmail.com>
2018-12-09 15:57:20 +0100

    Remove redundant files
    The functionality of these files was clustered to avoid code repetition

--- src/habilities/__internals__/manager/index.js
+++ src/habilities/__internals__/manager/index.js
@@ -1,19 +1,8 @@
-const entityFiles = require('./createEntityFiles');
-const layerFiles = require('./createLayerFiles');
 const indexFiles = require('./createIndexFile');
-const useCaseFiles = require('./createUseCaseFiles');
 const createLayer = require('./createLayer');
 const helpers = require('./helpers');
 
 module.exports = ({
-  create: {
-    entityFiles,
-    indexFiles,
-    interactorFiles: layerFiles.interactors,
-    storeFiles: layerFiles.stores,
-    useCaseFiles,
-  },
-
   createLayer: {
     useCases: createLayer.useCases,
     interactors: createLayer.interactors,
