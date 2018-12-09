5bff17b78370cb6637fc509cf19a257c74a0ef19
Eduardo Anaya <gran_ed@hotmail.com>
2018-12-10 00:42:59 +0100

    add initial forge implementation
    this commit adds some functionality to forge  function. The domain init is still pending

--- src/habilities/forge.js
+++ src/habilities/forge.js
@@ -1,5 +1,14 @@
-function forge(/* blueprint, target */) {
-  throw new Error('Implement me');
+const manager = require('./__internals__/manager');
+
+function forge(resources, path) {
+  Object.keys(resources).forEach((layer) => {
+    const layerDefinition = resources[layer];
+    const mapper = manager.helpers.createPlaceHolderMapper(layer, layerDefinition);
+    const fileNameList = manager.createLayer[layer](path, mapper);
+    manager.createIndex(path, layer, fileNameList);
+  });
+
+  console.log('TODO: create domain file');
 }
 
 module.exports = forge;
