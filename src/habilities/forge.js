73366b7d7ebaff9ec291c1ee55b8e4e6d86250b8
Eduardo Anaya <gran_ed@hotmail.com>
2018-12-10 01:16:45 +0100

    implement forge hability 💪🏾

--- src/habilities/forge.js
+++ src/habilities/forge.js
@@ -1,14 +1,24 @@
 const manager = require('./__internals__/manager');
 
-function forge(resources, path) {
+function createDomain(targetPath, resources) {
   Object.keys(resources).forEach((layer) => {
     const layerDefinition = resources[layer];
     const mapper = manager.helpers.createPlaceHolderMapper(layer, layerDefinition);
-    const fileNameList = manager.createLayer[layer](path, mapper);
-    manager.createIndex(path, layer, fileNameList);
+    const fileNameList = manager.createLayer[layer](targetPath, mapper);
+    manager.createIndex(targetPath, layer, fileNameList);
   });
 
   console.log('TODO: create domain file');
+}
+
+async function forge({ target, blueprint: blueprintPath }) {
+  try {
+    const blueprint = await manager.readBluePrint.fromJson(blueprintPath);
+    createDomain(target, blueprint);
+  } catch (error) {
+    // eslint-disable-next-line
+    console.log('Error while forging!', error);
+  }
 }
 
 module.exports = forge;
