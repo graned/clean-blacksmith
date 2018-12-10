650f24ab0e940e07e736b9f199e42e1dbe5aac01
Eduardo Anaya <gran_ed@hotmail.com>
2018-12-10 01:39:28 +0100

    add initial CLI tool

---
+++ cli/blacksmith-cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const commandLineArgs = require('command-line-args');
+const blacksmith = require('../src/blacksmith');
+
+let mergeOptions = {};
+
+const blackSmithDefinitions = [
+  { name: 'command', defaultOption: true },
+];
+
+const mainOptions = commandLineArgs(blackSmithDefinitions, { stopAtFirstUnknown: true });
+// eslint-disable-next-line
+const argv = mainOptions._unknown || [];
+
+/* second - parse the merge command options */
+if (mainOptions.command === 'forge') {
+  const mergeDefinitions = [
+    { name: 'target', alias: 't', type: String },
+    { name: 'blueprint', alias: 'b', type: String },
+  ];
+
+  mergeOptions = commandLineArgs(mergeDefinitions, { argv });
+}
+
+// eslint-disable-next-line
+blacksmith(mainOptions.command, mergeOptions).catch(console.log);
